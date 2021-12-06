960f4c3c5cfd2f9ec0002aa1c0a6fb9856e988cf
Heoseongmin <heosm0402@gmail.com>
2021-12-06 18:12:14 +0900

    [Feature] New attribute for scoring
    New Map type attribute for record score.

--- src/com/smheo/YachtGame/GameController.java
+++ src/com/smheo/YachtGame/GameController.java
@@ -1,7 +1,12 @@
 package com.smheo.YachtGame;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameController {
     private int round = 0;
+    private Map<String, Integer> score_p1 = new HashMap<>();
+    private Map<String, Integer> score_p2 = new HashMap<>();
 
     public void run() {
         NewPlayer p1 = new NewPlayer();
@@ -10,11 +15,12 @@ public class GameController {
         p2.setPlayerNumber(2);
 
         while (this.round < 12) {
-            System.out.println("[GAME INFO] ROUND" + this.round);
+            System.out.println("[GAME INFO] ROUND " + this.round);
             p1.turn();
             p2.turn();
             System.out.println("[GAME INFO] TOTAL SCORE P1:" + p1.getTotalScore() +
                     " | P2:" + p2.getTotalScore());
+            System.out.println("==================================================");
             this.round += 1;
         }
     }
