6ee1d353834e4769fb01a351643b8b41c18768f1
Heoseongmin <heosm0402@gmail.com>
2021-12-06 16:27:04 +0900

    [Feature] Implement method 'rollDice'
    Roll dice and save value in attrib 'dicesArray'.

--- src/com/smheo/YachtGame/NewPlayer.java
+++ src/com/smheo/YachtGame/NewPlayer.java
@@ -3,6 +3,7 @@ package com.smheo.YachtGame;
 public class NewPlayer {
     private int playerNumber = 0;
     private int totalScore = 0;
+    private int[] dicesArray = {0, 0, 0, 0, 0};
 
     public int getPlayerNumber() {
         return playerNumber;
@@ -22,9 +23,16 @@ public class NewPlayer {
 
     public void turn() {
         System.out.println("[GAME INFO] PLAYER " + this.playerNumber + " TURN");
+        rollDice();
     }
 
     private void rollDice() {
-
+        for (int i = 0; i < dicesArray.length; i++) {
+            if (dicesArray[i] == 0) {
+                double randomValue = Math.random();
+                int intValue = (int) (randomValue * 6) + 1;
+                dicesArray[i] = intValue;
+            }
+        }
     }
 }
