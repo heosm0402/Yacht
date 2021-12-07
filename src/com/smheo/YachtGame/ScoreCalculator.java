cdf90579cc238aa2220a438067abbbf774531469
Heoseongmin <heosm0402@gmail.com>
2021-12-07 19:33:31 +0900

    [Chore] Edit class name

--- src/com/smheo/YachtGame/ScoreCalculator.java
+++ src/com/smheo/YachtGame/ScoreCalculator.java
@@ -1,10 +1,9 @@
 package com.smheo.YachtGame;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ScoreController {
+public class ScoreCalculator {
     private String rank;
     private int score;
 
