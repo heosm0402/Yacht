a640f3df2fc8b8207499039b0d34e6906213ada2
Heoseongmin <heosm0402@gmail.com>
2021-12-09 15:46:03 +0900

    [Feature] Main script for game

---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+
+import com.smheo.YachtGame.GameController;
+
+public class Main {
+    public static void main(String[] args) {
+        GameController Yacht = new GameController();
+        Yacht.run();
+    }
+}
